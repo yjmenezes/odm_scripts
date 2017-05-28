7ddb68479d7a4526044debc23ba7089d6db2ac94
julio cesar de menezes <yjmenezes@hotmail.com>
2017-05-28 09:57:23 -0300

    Add files via upload
    transform ODM gcp_list.txt from WGS84 Lon/Lat to  WGS84 UTM.

---
+++ proj4_ddeg2utm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#GPL2 jmenezes example proj4, from lon/lat to utm wgs; 2017-05-28
+#EPSG:4326
+#73.218063 0.225385 0 1726 2916 R0011006.jpg A
+if [ $# -lt 2 ]; then
+    echo $(basename $0)" zone  [S|N] < gcp_lonlat.txt > gcp_utm.txt"
+    exit 1
+fi
+Z=$1
+case $2 in
+s|S)
+grep \- | proj  -f "%.3f" +proj=utm +zone=$Z +south +ellps=WGS84 | \
+awk -v z=$Z 'BEGIN { print "EPSG:" z+32700 } { print $0 }'
+;;
+n|N)
+grep ^[0-9] | proj  -f "%.3f" +proj=utm +zone=$Z +north +ellps=WGS84 | \
+awk -v z=$Z 'BEGIN { print "EPSG:" z+32600 } { print $0 }'
+;;
+*)
+;;
+esac
+exit 0
