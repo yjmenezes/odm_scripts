ee8d32b1b31d04fba687801fc41d9f633b858477
julio cesar de menezes <yjmenezes@hotmail.com>
2017-07-11 12:31:57 -0300

    Add files via upload
    bugfix:  tab as field separator

--- odm_exif2utm.sh
+++ odm_exif2utm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #GPL2 jmenezes ODM extract exif lon/lat project to utm with proj4; 2017-05-28
+# line 23  tab bugfix 2017-07-11
 if [ $# -lt 2 ]; then
     echo "run inside /images/ directory" 1>&2
     echo $(basename $0)" zone  [S|N] > camera_wgs84utm.txt" 1>&2
@@ -19,5 +20,5 @@ for i in *[jpg,JPG,tif,TIF]; do
     awk -F \: -v img=$i '{ print $2","img }' | tr -d [:blank:] | \
     sed s/deg/d/g | tr \, \\t | awk '{ print $2,$1,$3 }' | \
     proj  -f "%.3f" +proj=utm +zone=$Z +$H +ellps=WGS84 
-done
+done | sed s/\ /\\t/g
 exit 0
